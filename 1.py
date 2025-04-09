9b1ef6cb0cf446ab3771f366874b61a89d291ff4
Daniel_drr <dandelrui02@iesfidiana.es>
2025-04-09 20:51:35 +0200

    “Otra línea fichero python 1 desde VSC rama master”

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
 print("fichero 1")
 print("Añadiendo nueva linea fichero python 1 desde VSC")
 print("Agrego nueva línea fichero python 1 desde VSC rama master")
+print("Otra línea fichero python 1 desde VSC rama master")
